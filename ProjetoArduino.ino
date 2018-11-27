dfc08c59e600fc4448e26a2865f783cafa1b327b
Jean Souza <jean.carlos.nmk@gmail.com>
2018-11-27 16:36:01 -0200

    removendo arquivos desnecessarios e atualizando o ProjetoArduino

--- ProjetoArduino.ino
+++ ProjetoArduino.ino
@@ -1,5 +1,7 @@
 #include <SPI.h>
 #include <Ethernet.h>
+String readString;
+int numeroPedidos = 0;
 
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
@@ -8,10 +10,16 @@ IPAddress ip(192, 168, 97, 242);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress gateway(192, 168, 97, 1);
 
+int ledVerde = 7;
+int ledVermelho = 6;
+
 EthernetServer server(80);
 
 void setup() {
   Serial.begin(9600);  
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledVermelho, OUTPUT);
+
   while (!Serial) {
     ;
   }
@@ -43,8 +51,37 @@ void loop() {
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        Serial.write(c);
+        
+        //read char by char HTTP request
+        if (readString.length() < 100) {
+
+          //store characters to string
+          readString += c;
+          Serial.print(c); //print what server receives to serial monitor
+        }
+        //Serial.write(c);
+        
         if (c == '\n' && currentLineIsBlank) {
+          Serial.println(readString);
+          //Serial.println(numeroPedidos);
+          //numeroPedidos = readString.indexOf("?pedid");
+
+
+          Serial.println(readString.indexOf("?pedidoNovo"));
+          if (readString.indexOf("?pedidoNovo") > 0){
+              digitalWrite(ledVermelho, HIGH);
+              delay(1000);
+              digitalWrite(ledVermelho, LOW);
+          }
+
+          if (readString.indexOf("?pedidoPronto") > 0) {
+              digitalWrite(ledVerde, HIGH);
+              delay(1000);
+              digitalWrite(ledVerde, LOW);
+          }
+
+          readString = "";
+          
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");
@@ -53,23 +90,22 @@ void loop() {
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
           client.println("<head>");
-          client.println("<title>Bootstrap Example</title>");
-          client.println("<meta charset='utf-8'>");
-          client.println("<meta name='viewport' content='width=device-width, initial-scale=1'>");
-          client.println("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'>");
-          client.println("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>");
-          client.println("<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'></script>");
-          client.println("<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'></script>");
+          client.println("  <title>iComidaCozinha</title>");
+          client.println("  <meta charset='utf-8'>");
+          client.println("  <meta name='viewport' content='width=device-width, initial-scale=1'>");
+          client.println("  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'>");
+          client.println("  <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>");
+          client.println("  <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'></script>");
+          client.println("  <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'></script>");
           client.println("</head>");
           client.println("<body>");
           client.println("<div class='jumbotron text-center bg-danger text-white'>");
-          client.println("<h1><strong>iComida</strong> Cozinha</h1>");
-          client.println("<p>Gerencie o status dos pedidos aqui!</p>");
+          client.println("  <h1><strong>iComida</strong> Cozinha</h1>");
+          client.println("  <p>Gerencie o status dos pedidos aqui!</p>");
           client.println("</div>");
-          client.println("</div>");
+          client.println("<button class='btn btn-warning btn-atualizar' style='margin: 0 0 15px 15px'>Atualizar</button>");
           client.println("<div id='pedidos'></div>");
-          //client.println("<iframe src='https://www.w3schools.com'></iframe> ");
-          client.println("<script type='text/js'> var html =  $('#pedidos').load('http://187.255.163.75:8080/iComidaREST/rest/icomida/pedidos').html(); console.log(html);</script>");
+          client.println("<script src='http://187.255.165.251:8080/iComidaREST/rest/icomida/downloadScriptArchive/'></script>");  
           client.println("</body>");
           client.println("</html>");
           break;
